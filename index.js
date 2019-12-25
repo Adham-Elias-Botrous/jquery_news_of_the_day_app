8a2cabdaada9f18869a0ef9979c5dda838e2da8a
Adham Elias Botrous <50738429+Adham-Elias-Botours@users.noreply.github.com>
2019-12-25 17:10:36 +0100

    ready for git pages

---
+++ index.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  var url = 'http://hn.algolia.com/api/v1/search?query=javascript';
+  $.getJSON(url, function(data) {
+    var currentQuote = '';
+    var quotes = data.hits;
+    $('.read').on('click', function() {
+      currentQuote = quotes[Math.floor(Math.random() * quotes.length)];
+      $('.quoteTitle').html(
+        `<a href=${currentQuote.url} target='blank'>${currentQuote.title}</a>`
+      );
+      $('.quoteAuthor').html(
+        'Author: <strong>' + currentQuote.author + '</strong>'
+      );
+      $('.tweetQuote')
+        .attr('target', '_blank')
+        .attr(
+          'href',
+          `https://twitter.com/intent/tweet?text=${currentQuote.title}%0a${currentQuote.url}`
+        );
+      $('.tweetQuote').removeClass('disabled');
+      $('.read').text('Next news');
+      console.log(currentQuote);
+    });
+  });
+});
